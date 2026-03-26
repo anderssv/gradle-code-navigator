81ec77060389807e34b541c0de264cc157df0911
Anders Sveen <anders@f12.no>
2026-03-26 21:27:42 +0100

    - r extract FileCache<T> abstract base for cache consolidation

---
+++ src/core/kotlin/no/f12/codenavigator/navigation/FileCache.kt
@@ -0,0 +1,30 @@
+package no.f12.codenavigator.navigation
+
+import no.f12.codenavigator.CacheFreshness
+import java.io.File
+
+abstract class FileCache<T> {
+
+    protected val FIELD_SEPARATOR = "\t"
+
+    abstract fun write(cacheFile: File, data: T)
+    abstract fun read(cacheFile: File): T
+    protected abstract fun build(classDirectories: List<File>): ScanResult<T>
+
+    fun isFresh(cacheFile: File, classDirectories: List<File>): Boolean =
+        CacheFreshness.isFresh(cacheFile, classDirectories)
+
+    fun getOrBuild(cacheFile: File, classDirectories: List<File>): ScanResult<T> {
+        if (isFresh(cacheFile, classDirectories)) {
+            try {
+                return ScanResult(read(cacheFile), emptyList())
+            } catch (_: Exception) {
+                cacheFile.delete()
+            }
+        }
+
+        val result = build(classDirectories)
+        write(cacheFile, result.data)
+        return result
+    }
+}
